94d97ffa2a19b654fa4ce5728e22c83629daee1d
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-01-19 13:13:58 -0500

    task 13. Binding + Closure

---
+++ 0x0C-Javascript_advanced/13-bind_user.js
@@ -0,0 +1,30 @@
+/*
+    Create a function named logWelcomeUser:
+
+    - It takes one argument welcomeString (String)
+    - It logs to the console <welcomeString>, <firstName>. Your occupation is: <occupation>
+
+    Create a variable named bindLogWelcomeUser. It binds the logWelcomeUser function to the user object.
+    Call the function with the string Welcome
+
+    Requirements:
+
+    - Calling bindLogWelcomeUser('Hello'); should return Hello, Buillaume. Your occupation is: Engineer
+
+*/
+let user = {
+    hobby: "Calligraphy",
+    favoriteSport: "Hockey",
+    astrologicalSign: "Aries",
+    firstName: "Buillaume",
+    lastName: "Ialva",
+    location: "Telaviv",
+    occupation: "Engineer"
+}
+
+function logWelcomeUser(welcomeString){
+    console.log(`${welcomeString}, ${this.firstName}. Your occupation is: ${this.occupation}`);
+}
+
+let bindLogWelcomeUser = logWelcomeUser.bind(user);
+bindLogWelcomeUser('Welcome');
